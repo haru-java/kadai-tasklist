b0a2347806f5d71b7d89add7ed6bfdedff663f6d
haruka.iwabu <haruka.iwabu@techacademy.jp>
2023-07-30 15:27:39 +0900

    かえた

--- src/filters/EncodingFilter.java
+++ src/filters/EncodingFilter.java
@@ -26,24 +26,23 @@ public class EncodingFilter implements Filter {
      * Default constructor.
      */
     public EncodingFilter() {
-        // TODO Auto-generated constructor stub
+
     }
 
     /**
      * @see Filter#destroy()
      */
     public void destroy() {
-        // TODO Auto-generated method stub
+
     }
 
     /**
      * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
      */
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        // TODO Auto-generated method stub
-        // place your code here
+        request.setCharacterEncoding("UTF-8");
+        response.setCharacterEncoding("UTF-8");
 
-        // pass the request along the filter chain
         chain.doFilter(request, response);
     }
 
@@ -51,7 +50,7 @@ public class EncodingFilter implements Filter {
      * @see Filter#init(FilterConfig)
      */
     public void init(FilterConfig fConfig) throws ServletException {
-        // TODO Auto-generated method stub
+
     }
 
 }
